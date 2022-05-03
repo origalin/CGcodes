81c3a0d4b179c301a31f96c330f458dc5641c715
origalin <2360436350@qq.com>
2022-05-02 22:00:39 -0500

    act2

--- 2021.10.15.Quaternions/QuaternionStarter/HelloQuaternion.js
+++ 2021.10.15.Quaternions/QuaternionStarter/HelloQuaternion.js
@@ -169,4 +169,168 @@ var R1 = new Matrix4();
 	*  (Be sure you try the quaternion multiply vs. matrix multiply)
 	*
 	*/
+  testQuaternions()
 	}
+function testQuaternions() {
+//==============================================================================
+// Test our little "quaternion-mod.js" library with simple rotations for which
+// we know the answers; print results to make sure all functions work as
+// intended.
+// 1)  Test constructors and value-setting functions:
+  var mtx = new Matrix4();
+  var res = 5;
+  var myQuat = new Quaternion(1,2,3,4);
+  console.log('constructor: myQuat(x,y,z,w)=',
+    myQuat.x, myQuat.y, myQuat.z, myQuat.w);
+  console.log("show in matrix")
+  mtx.setFromQuat(myQuat.x, myQuat.y, myQuat.z, myQuat.w)
+  mtx.printMe()
+  myQuat.clear();
+  console.log('myQuat.clear()=',
+    myQuat.x.toFixed(res), myQuat.y.toFixed(res),
+    myQuat.z.toFixed(res), myQuat.w.toFixed(res));
+  console.log("show in matrix")
+  mtx.setFromQuat(myQuat.x, myQuat.y, myQuat.z, myQuat.w)
+  mtx.printMe()
+  myQuat.set(1,2, 1,2);
+  console.log('myQuat.set(1,2,1,2)=',
+    myQuat.x.toFixed(res), myQuat.y.toFixed(res),
+    myQuat.z.toFixed(res), myQuat.w.toFixed(res));
+  console.log('myQuat.length()=', myQuat.length().toFixed(res));
+  console.log("show in matrix")
+  mtx.setFromQuat(myQuat.x, myQuat.y, myQuat.z, myQuat.w)
+  mtx.printMe()
+  myQuat.normalize();
+  console.log('myQuat.normalize()=',
+    myQuat.x.toFixed(res), myQuat.y.toFixed(res), myQuat.z.toFixed(res), myQuat.w.toFixed(res));
+  console.log("show in matrix")
+  mtx.setFromQuat(myQuat.x, myQuat.y, myQuat.z, myQuat.w)
+  mtx.printMe()
+  // Simplest possible quaternions:
+  myQuat.setFromAxisAngle(1,0,0,10);
+  console.log('Set myQuat to 10-deg. rot. on x axis=',
+    myQuat.x.toFixed(res), myQuat.y.toFixed(res), myQuat.z.toFixed(res), myQuat.w.toFixed(res));
+  console.log("show in matrix")
+  mtx.setFromQuat(myQuat.x, myQuat.y, myQuat.z, myQuat.w)
+  mtx.printMe()
+  console.log("compare to the same rotation of matrix")
+  mtx.setIdentity()
+  mtx.rotate(10,1,0,0)
+  mtx.printMe()
+  console.log("thet are the same")
+
+  myQuat.setFromAxisAngle(0,1,0,10);
+  console.log('set myQuat to 10-deg. rot. on y axis=',
+    myQuat.x.toFixed(res), myQuat.y.toFixed(res), myQuat.z.toFixed(res), myQuat.w.toFixed(res));
+  console.log("show in matrix")
+  mtx.setFromQuat(myQuat.x, myQuat.y, myQuat.z, myQuat.w)
+  mtx.printMe()
+  console.log("compare to the same rotation of matrix")
+  mtx.setIdentity()
+  mtx.rotate(10,0,1,0)
+  mtx.printMe()
+  console.log("thet are the same")
+
+  myQuat.setFromAxisAngle(0,0,1,10);
+  console.log('set myQuat to 10-deg. rot. on z axis=',
+    myQuat.x.toFixed(res), myQuat.y.toFixed(res), myQuat.z.toFixed(res), myQuat.w.toFixed(res), '\n');
+  console.log("show in matrix")
+  mtx.setFromQuat(myQuat.x, myQuat.y, myQuat.z, myQuat.w)
+  mtx.printMe()
+  console.log("compare to the same rotation of matrix")
+  mtx.setIdentity()
+  mtx.rotate(10,0,0,1)
+  mtx.printMe()
+  console.log("thet are the same")
+
+  myQmat = new Matrix4();
+  myQuat.setFromAxisAngle(1,0,0, 95.0);
+  console.log('set myQuat to +95-deg rot. on x axis =',
+    myQuat.x.toFixed(res), myQuat.y.toFixed(res), myQuat.z.toFixed(res), myQuat.w.toFixed(res));
+  myQmat.setFromQuat(myQuat.x, myQuat.y, myQuat.z, myQuat.w);
+  console.log('myQuat as matrix: (+y axis <== -z axis)(+z axis <== +y axis)');
+  myQmat.printMe();
+  console.log("compare to the same rotation of matrix")
+  mtx.setIdentity()
+  mtx.rotate(95,1,0,0)
+  mtx.printMe()
+  console.log("thet are the same")
+
+  myQuat.setFromAxisAngle(0,1,0, 95.0);
+  console.log('set myQuat to +95-deg rot. on y axis =',
+    myQuat.x.toFixed(res), myQuat.y.toFixed(res), myQuat.z.toFixed(res), myQuat.w.toFixed(res));
+  myQmat.setFromQuat(myQuat.x, myQuat.y, myQuat.z, myQuat.w);
+  console.log('myQuat as matrix: (+x axis <== +z axis)(+z axis <== -x axis)');
+  myQmat.printMe();
+  console.log("compare to the same rotation of matrix")
+  mtx.setIdentity()
+  mtx.rotate(95,0,1,0)
+  mtx.printMe()
+  console.log("thet are the same")
+
+  myQuat.setFromAxisAngle(0,0,1, 95.0);
+  console.log('set myQuat to +90-deg rot. on z axis =',
+    myQuat.x.toFixed(res), myQuat.y.toFixed(res), myQuat.z.toFixed(res), myQuat.w.toFixed(res));
+  myQmat.setFromQuat(myQuat.x, myQuat.y, myQuat.z, myQuat.w);
+  console.log('myQuat as matrix: (+x axis <== -y axis)(+y axis <== +x axis)');
+  myQmat.printMe();
+  console.log("compare to the same rotation of matrix")
+  mtx.setIdentity()
+  mtx.rotate(95,0,0,1)
+  mtx.printMe()
+  console.log("thet are the same")
+
+  // Test quaternion multiply:
+  // (q1*q2) should rotate drawing axes by q1 and then by q2;  it does!
+  var qx90 = new Quaternion;
+  var qy90 = new Quaternion;
+  qx90.setFromAxisAngle(1,0,0,45.0);			// +90 deg on x axis
+  qy90.setFromAxisAngle(0,1,0,45.0);			// +90 deg on y axis.
+  myQuat.multiply(qx90,qy90);
+  console.log('set myQuat to (45deg x axis) * (45deg y axis) = ',
+    myQuat.x.toFixed(res), myQuat.y.toFixed(res), myQuat.z.toFixed(res), myQuat.w.toFixed(res));
+  myQmat.setFromQuat(myQuat.x, myQuat.y, myQuat.z, myQuat.w);
+  console.log('myQuat as matrix: (+x <== +z)(+y <== +x )(+z <== +y');
+  myQmat.printMe();
+  console.log("compare to the same rotation of matrix")
+  mtx.setIdentity()
+  mtx.rotate(45,1,0,0)
+  mtx.rotate(45,0,1,0)
+  mtx.printMe()
+  console.log("thet are the same")
+
+  // What is new:
+  var aMat = new Matrix4();
+  aMat.setIdentity();
+  var mySiz = 3000;
+  var count;
+
+  console.log('----------------------------------')
+  console.log('Rotate aMat by (360/'+mySiz+') degrees\n around the (1,3,5) axis,'+mySiz+' times:');
+  for(count = 0; count < mySiz; count++) {
+    aMat.rotate(-360.0/mySiz, 1.0, 3.0, 5.0);
+  }
+  console.log('Result SHOULD be a perfect identity matrix, but it is not:');
+
+  aMat.printMe();
+  var myQuat = new Quaternion(0,0,0,1);
+  myQmat.setFromQuat(myQuat.x, myQuat.y, myQuat.z, myQuat.w);
+  console.log('But if we rotate a Quaternion by (360/'+mySiz+') degrees\n around the (1,3,5) axis,'+mySiz+' times:');
+  console.log('Before:')
+  myQmat.printMe();
+  for(count = 0; count < mySiz; count++) {
+    var qNew = new Quaternion
+    qNew.setFromAxisAngle(1,3,5,360/mySiz)
+    var qTmp = new Quaternion
+    qTmp.multiply(myQuat, qNew);
+    myQuat.copy(qTmp)
+  }
+  myQmat.setFromQuat(myQuat.x, myQuat.y, myQuat.z, myQuat.w);
+  console.log('After:')
+  myQmat.printMe();
+  console.log('Identical!!!')
+  console.log('Just like this example, in our code at projectA, the rotations are applied many times due to the animation frame.' +
+    'It will bring error when mouse is moving.')
+  console.log('This accumulated error causes the actual axis after the drag shifted little by little and will not reflect our actual action anymore.')
+
+}
